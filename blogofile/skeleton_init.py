48f129def1f042bcbb96345790a8465bffb56e16
Ryan McGuire <ryan@Sovreign.(none)>
2009-08-14 14:31:47 -0400

    fix feed links in skeleton to be relative to blog_path

--- blogofile/skeleton_init.py
+++ blogofile/skeleton_init.py
@@ -57,8 +57,8 @@ __site_mako = """<%inherit file="base.mako" />
 """
 
 __head_mako = """<title>${config.blog_name}</title>
-<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="/feed" />
-<link rel="alternate" type="application/atom+xml" title="Atom 1.0" href="/feed/atom" />
+<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="${config.blog_path}/feed" />
+<link rel="alternate" type="application/atom+xml" title="Atom 1.0" href="${config.blog_path}/feed/atom" />
 """
 
 __header_mako = """<h1><a href="/">${config.blog_name}</a></h1>
