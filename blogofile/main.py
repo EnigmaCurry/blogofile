37132970623c20d08d9d3ec3a97171809cdc2088
Ron DuPlain <ron.duplain@gmail.com>
2009-08-17 12:57:57 -0400

    Added `blogofile help`. Use `blogofile help` and `blogofile help commands`

--- blogofile/main.py
+++ blogofile/main.py
@@ -10,12 +10,12 @@ Definition: Blogophile --
 Definition: Blogofile  --
  A static file blog engine/compiler, inspired by Jekyll.
 
-Blogofile transforms a set of templates into an entire blog consisting of static
-HTML files. All categories, tags, RSS/Atom feeds are automatically maintained by
-Blogofile. This blog can be hosted on any HTTP web server. Since the blog is
-just HTML, CSS, and Javascript, no CGI environment, or database is required.
-With the addition of a of third-party comment and trackback provider (like
-Disqus or IntenseDebate) a modern and interactive blog can be hosted very
+Blogofile transforms a set of templates into an entire blog consisting of
+static HTML files. All categories, tags, RSS/Atom feeds are automatically
+maintained by Blogofile. This blog can be hosted on any HTTP web server. Since
+the blog is just HTML, CSS, and Javascript, no CGI environment, or database is
+required.  With the addition of a of third-party comment and trackback provider
+(like Disqus or IntenseDebate) a modern and interactive blog can be hosted very
 inexpensively.
 
 Please take a moment to read LICENSE.txt. It's short.
@@ -25,12 +25,8 @@ __author__  = "Ryan McGuire (ryan@enigmacurry.com)"
 from blogofile import __version__ 
 
 import logging
-logging.basicConfig()
-logger = logging.getLogger('main')
-
 import os
 import sys
-import shlex
 
 import argparse
 import pygments.formatters
@@ -41,86 +37,109 @@ from writer import Writer
 import config
 import skeleton_init
 
-def get_options(cmd=None):
-    parser = argparse.ArgumentParser()
-    parser.version = "Blogofile " +__version__+ " -- http://www.blogofile.com"
-    parser.add_argument("-c","--config-file",dest="config_file",
+logging.basicConfig()
+logger = logging.getLogger("main")
+
+parser = argparse.ArgumentParser()
+parser.version = "Blogofile " + __version__ + " -- http://www.blogofile.com"
+subparsers = parser.add_subparsers()
+
+def get_options():
+    parser.add_argument("-c", "--config-file", dest="config_file",
                         help="config file to load (default './_config.py')",
                         metavar="FILE", default="./_config.py")
-    parser.add_argument("-V","--version",action="version")
-    parser.add_argument("-v","--verbose",dest="verbose",default=False,
+    parser.add_argument("-V", "--version", action="version")
+    parser.add_argument("-v", "--verbose", dest="verbose", default=False,
                         action="store_true", help="Enable extra verboseness")
-    subparsers = parser.add_subparsers()
 
-    parser_build = subparsers.add_parser('build',
-                                         help="Build the blog from source")
-    parser_build.add_argument("--include-drafts", dest="include_drafts",
-                              default=False, action="store_true",
-                              help="Writes permapages for drafts "
-                              "(but not in feeds or chronlogical blog)")
-    parser_build.set_defaults(func=do_build)
+    p_help = subparsers.add_parser("help", help="Show help for a command",
+                                   add_help=False)
+    p_help.add_argument("command", help="a blogofile subcommand e.g. build",
+                        nargs="*", default="none")
+    p_help.set_defaults(func=do_help)
 
-    parser_init = subparsers.add_parser('init',
-                                        help="Create a minimal blogofile "
-                                        "configuration in the current "
-                                        "directory")
-    parser_init.set_defaults(func=do_init)
+    p_build = subparsers.add_parser("build", help="Build the blog from source")
+    p_build.add_argument("--include-drafts", dest="include_drafts",
+                         default=False, action="store_true",
+                         help="Writes permapages for drafts "
+                         "(but not in feeds or chronlogical blog)")
+    p_build.set_defaults(func=do_build)
 
-    parser_serve = subparsers.add_parser("serve",
-                                         help="Host the _site dir with the "
-                                         "builtin webserver. Don't use this "
-                                         "outside of a firewall!")
-    parser_serve.add_argument("PORT", help="port on which to serve")
-    parser_serve.set_defaults(func=do_serve)
+    p_init = subparsers.add_parser("init", help="Create a minimal blogofile "
+                                   "configuration in the current directory")
+    p_init.set_defaults(func=do_init)
+
+    p_serve = subparsers.add_parser("serve", help="Host the _site dir with "
+                                    "the builtin webserver. Don't use this "
+                                    "outside of a firewall!")
+    p_serve.add_argument("PORT", help="port on which to serve")
+    p_serve.set_defaults(func=do_serve)
 
     if len(sys.argv) <= 1:
         parser.print_help()
-        sys.exit(1)
+        parser.exit(1)
 
-    if not cmd:
-        args = parser.parse_args()
-    else:
-        args = shlex.split(cmd)
-        (options, args) = parser.parse_args(args)
+    args = parser.parse_args()
     return (parser, args)
 
-def main(cmd=None):
-    (parser, args) = get_options(cmd)
-    options = args
+def main():
+    parser, args = get_options()
 
-    if options.verbose:
+    if args.verbose:
         logger.setLevel(logging.DEBUG)
         logger.info("Setting DEBUG mode")
 
-    options.config_dir = os.path.split(os.path.abspath(options.config_file))[0]
-    if not os.path.isdir(options.config_dir):
-        print("config dir does not exist : %s" % options.config_dir)
+    args.config_dir = os.path.split(os.path.abspath(args.config_file))[0]
+    if not os.path.isdir(args.config_dir):
+        print("config dir does not exist : %s" % args.config_dir)
         sys.exit(1)
-    os.chdir(options.config_dir)
+    os.chdir(args.config_dir)
 
-    args.func(options)
+    args.func(args)
 
-def do_serve(options):
+def do_help(args):
+    if "commands" in args.command:
+        args.command = sorted(subparsers.choices.keys())
+
+    if "none" in args.command:
+        parser.print_help()
+    else:
+        # Where did the subparser help text go? Let's get it back.
+        # Certainly there is a better way to retrieve the helptext than this...
+        helptext = {}
+        for subcommand in args.command:
+            for action in subparsers._choices_actions:
+                if action.dest == subcommand:
+                    helptext[subcommand] = action.help
+                    break
+            else:
+                helptext[subcommand] = ""
+        # Print help for each subcommand requested.
+        for subcommand in args.command:
+            print >>sys.stderr, "%s - %s" % (subcommand, helptext[subcommand])
+            subparsers.choices[subcommand].print_help()
+            print >>sys.stderr, "\n"
+
+def do_serve(args):
     os.chdir("_site")
     import SimpleHTTPServer
-    sys.argv = [None, options.do_serve]
+    sys.argv = [None, args.do_serve]
     SimpleHTTPServer.test()
 
-def do_build(options):
+def do_build(args):
     #load config
     try:
-        config.init(options.config_file)
+        config.init(args.config_file)
     except config.ConfigNotFoundException:
-        print("No configuration found at : %s" % options.config_file)
-        print("If you want to make a new site, try `blogofile init`")
-        return
+        print >>sys.stderr, ("No configuration found: %s" % args.config_file)
+        parser.exit(1, "Want to make a new site? Try `blogofile init`\n")
 
     logger.info("Running user's pre_build() function..")
     writer = Writer(output_dir="_site")
     if config.blog_enabled == True:
         config.pre_build()
         posts = post.parse_posts("_posts")
-        if options.include_drafts:
+        if args.include_drafts:
             drafts = post.parse_posts("_drafts", config)
             for p in drafts:
                 p.draft = True
@@ -133,8 +152,8 @@ def do_build(options):
     logger.info("Running user's post_build() function..")
     config.post_build()
 
-def do_init(options):
-    skeleton_init.do_init(options)
+def do_init(args):
+    skeleton_init.do_init(args)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
