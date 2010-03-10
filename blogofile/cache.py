cbd0821809d99afb504bc780654120d258b35cb7
Ryan McGuire <ryan@enigmacurry.com>
2010-03-10 01:00:50 -0500

    Start of a hierarchical cache object.
    Eventually, blogofile's _config.py may be able to support more structured hierarchical configuration options

--- blogofile/cache.py
+++ blogofile/cache.py
@@ -1,10 +1,24 @@
 import sys
 
 class Cache(dict):
-    """A dummy object used for attatching things we want to remember"""
+    """A dummy object used for attatching things we want to remember
+
+    >>> c = Cache()
+    >>> c.whatever = "whatever"
+    >>> c.whatever
+    'whatever'
+    >>> c.section.subsection.attribute = "whatever"
+    >>> c.section.subsection.attribute
+    'whatever'
+    """
     def __init__(self,**kw):
         dict.__init__(self,kw)
         self.__dict__ = self
+    def __getattr__(self, attr):
+        if not attr.startswith("_"):
+            c = Cache()
+            self.__dict__[attr] = c
+            return c
 
 #The main blogofile cache object, transfers state between templates
 bf = Cache()
