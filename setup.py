7c105474ab912e793d0b4836609c1d64973c1741
Ryan McGuire <ryan@enigmacurry.com>
2010-03-21 23:47:38 -0400

    require mechanize for functional tests

--- setup.py
+++ setup.py
@@ -17,7 +17,8 @@ setup(name='Blogofile',
                          'textile',
                          'markdown<2.0',
                          'argparse',
-                         'pygments'],
+                         'pygments',
+                         'mechanize'],
       entry_points="""
       [console_scripts]
       blogofile = blogofile.main:main
