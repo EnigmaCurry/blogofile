7278d9cdcb75db3806060a6377541fc1db6be780
Ryan McGuire <ryan@enigmacurry.com>
2010-03-21 22:25:12 -0400

    fix webserver to work in unit tests
    Added a blank unit test for the server

--- blogofile/server.py
+++ blogofile/server.py
@@ -15,18 +15,14 @@ class Server(threading.Thread):
         self.port = int(port)
         threading.Thread.__init__(self)
         self.is_shutdown = False
-    def run(self):
-        #We're guaranteed to be in the blogofile root after main runs
-        #So change to the _site dir that should be built directly beneath it:
-        #os.chdir("_site")
         server_address = ('',self.port)
         HandlerClass = BlogofileRequestHandler
         ServerClass = BaseHTTPServer.HTTPServer
         HandlerClass.protocol_version="HTTP/1.0"
         self.httpd = ServerClass(server_address, HandlerClass)
-        sa = self.httpd.socket.getsockname()
-        print("Blogofile server started on port %s ..." % sa[1])
-        #Start the server in a seperate thread
+        self.sa = self.httpd.socket.getsockname()
+    def run(self):
+        print("Blogofile server started on port %s ..." % self.sa[1])
         self.httpd.serve_forever()
     def shutdown(self):
         print("\nshutting down webserver...")
@@ -34,8 +30,10 @@ class Server(threading.Thread):
         self.httpd.socket.close()
         self.is_shutdown = True
         #TODO: why doesn't this actually shut it down?
-        
-BLOGOFILE_SUBDIR_ERROR = """\
+
+class BlogofileRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def __init__(*args, **kwargs):
+        self.BLOGOFILE_SUBDIR_ERROR = """\
 <head>
 <title>Error response</title>
 </head>
@@ -46,13 +44,12 @@ for the root page? : <a href='"""+\
     urlparse(config.site_url).path + "'>"+urlparse(config.site_url).path+\
     "</a>\n"+\
     "</body>"
-
-class BlogofileRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+        SimpleHTTPServer.SimpleHTTPRequestHandler.__init__(self, args, kwargs)
     def translate_path(self, path):
         site_path = urlparse(config.site_url).path
         if(len(site_path.strip("/")) > 0 and
            not path.startswith(site_path)):
-            self.error_message_format = BLOGOFILE_SUBDIR_ERROR
+            self.error_message_format = self.BLOGOFILE_SUBDIR_ERROR
             return "" #Results in a 404
         p = SimpleHTTPServer.SimpleHTTPRequestHandler.translate_path(
             self, path)
