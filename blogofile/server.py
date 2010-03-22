ada80f0970259540e670f800ef10e7d7e8e7c47e
Ryan McGuire <ryan@enigmacurry.com>
2010-03-21 20:24:53 -0400

    built-in webserver now correctly handles sites built inside subdirectories.
    The site is built directly into the _site direrectory but is transparently served with it's full URL in the webserver.
    
    example:
    
    config.site_url = "http://www.yoursite.com/people/ryan"
    
    "blogofile build" builds the site into the _site directory directly (no people/ryan directory is there)
    
    But, when you run "blogofile serve" you must access the site from http://localhost:8080/people/ryan
    
    This preserves any absolute links that are used within the site.

--- blogofile/server.py
+++ blogofile/server.py
@@ -4,7 +4,10 @@ import logging
 import os
 import re
 from urlparse import urlparse
+<<<<<<< HEAD:blogofile/server.py
 import threading
+=======
+>>>>>>> built-in webserver now correctly handles sites built inside subdirectories.:blogofile/server.py
 
 from blogofile import config, util
 
@@ -30,10 +33,15 @@ class Server(threading.Thread):
         self.httpd.socket.close()
         self.is_shutdown = True
         #TODO: why doesn't this actually shut it down?
+<<<<<<< HEAD:blogofile/server.py
 
 class BlogofileRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         self.BLOGOFILE_SUBDIR_ERROR = """\
+=======
+        
+BLOGOFILE_SUBDIR_ERROR = """\
+>>>>>>> built-in webserver now correctly handles sites built inside subdirectories.:blogofile/server.py
 <head>
 <title>Error response</title>
 </head>
@@ -44,6 +52,7 @@ for the root page? : <a href='"""+\
     urlparse(config.site_url).path + "'>"+urlparse(config.site_url).path+\
     "</a>\n"+\
     "</body>"
+<<<<<<< HEAD:blogofile/server.py
         SimpleHTTPServer.SimpleHTTPRequestHandler.__init__(self, *args, **kwargs)
     def translate_path(self, path):
         site_path = urlparse(config.site_url).path
@@ -64,3 +73,26 @@ for the root page? : <a href='"""+\
     
     def log_message(self, format, *args):
         pass
+=======
+
+class BlogofileRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def translate_path(self, path):
+        # If the site is configured to be hosted in a subdirectory
+        # override the SimpleHTTPServer default translate_path method
+        if len(urlparse(config.site_url).path.strip("/")) > 0:
+            return self.do_subdir_translate(path)
+        return SimpleHTTPServer.SimpleHTTPRequestHandler.translate_path(
+            self, path)
+    def do_subdir_translate(self, path):
+        site_path = urlparse(config.site_url).path
+        if(not path.startswith(site_path)):
+            self.error_message_format = BLOGOFILE_SUBDIR_ERROR
+            return "" #Results in a 404
+        p = SimpleHTTPServer.SimpleHTTPRequestHandler.translate_path(
+            self, path)
+        build_path = os.path.join(
+            os.getcwd(),
+            util.path_join(site_path.strip("/")))
+        p = re.sub(build_path, os.getcwd(), p)
+        return p
+>>>>>>> built-in webserver now correctly handles sites built inside subdirectories.:blogofile/server.py
