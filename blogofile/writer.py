ae1ccb72f35e5a5359cb25e25ccad414bc34096b
Nick Craig-Wood <nick@craig-wood.com>
2010-12-26 09:57:50 +0000

    Hardlink files instead of copying them for speed and space improvements
    Before
    
    $ sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'
    $ time blogofile build
    
    real	0m20.234s
    user	0m1.940s
    sys	0m0.260s
    
    After
    
    $ sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'
    $ time blogofile build
    
    real	0m16.444s
    user	0m1.590s
    sys	0m0.260s
    
    This also saves me > 80 MB of disk space!

--- blogofile/writer.py
+++ blogofile/writer.py
@@ -121,8 +121,12 @@ class Writer(object):
                     #Copy this non-template file
                     f_path = util.path_join(root, t_fn)
                     logger.debug("Copying file: " + f_path)
-                    shutil.copyfile(f_path,
-                            util.path_join(self.output_dir, f_path))
+                    out_path = util.path_join(self.output_dir, f_path)
+                    # Try hardlinking first, and if that fails copy
+                    try:
+                        os.link(f_path, out_path)
+                    except StandardError:
+                        shutil.copyfile(f_path, out_path)
 
     def __init_filters_controllers(self):
         #Run filter/controller defined init methods
