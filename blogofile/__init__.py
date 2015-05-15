1397cd734dea3c7a879d3c78457e7213ee9b819b
Ryan McGuire <ryan@enigmacurry.com>
2015-05-15 18:51:02 -0400

    0.8.2 release

--- blogofile/__init__.py
+++ blogofile/__init__.py
@@ -7,4 +7,4 @@ Please take a moment to read LICENSE.txt. It's short.
 """
 
 __author__ = "Ryan McGuire, Doug Latornell, and the Blogofile Contributors"
-__version__ = '0.8b1'
+__version__ = '0.8.2'
